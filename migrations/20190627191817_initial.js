8cb28dd478dfda02c433c9fc26aa882f0c13814d
Rane <rane@blasph.me>
2019-06-27 19:52:01 -0700

    initial migration

--- migrations/20190627191817_initial.js
+++ migrations/20190627191817_initial.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable('tasks', tbl => {
+      tbl.increments();
+      tbl.string('title', 255).notNullable();
+      tbl.boolean('completed').notNullable();
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('tasks');
 };
